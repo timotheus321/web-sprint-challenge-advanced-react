3aa75b394703f8e059dda8bad62b8606aa86e38b
Timothy White <bigtball321@yahoo.com>
2023-05-24 19:57:14 -0400

    finished

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -22,3 +22,36 @@ test('displays heading', () => {
    
    expect(coordinatesElement.textContent).toMatch(/\(2.*2\)$/)
 });
+
+
+test('moves left on click', () => {
+ 
+  render(<AppFunctional initialIndex={5} />);
+
+  const leftButton = screen.getByText(/LEFT/);
+  userEvent.click(leftButton);
+
+  const coordinatesElement = screen.getByTestId('coordinates');
+  expect(coordinatesElement.textContent).toMatch(/\(2.*2\)$/)
+});
+
+test('increments steps after move', async () => {
+  render(<AppFunctional initialIndex={5} />);
+  
+  const stepsElement = screen.getByText(/You moved 0 times/);
+  expect(stepsElement).toBeInTheDocument();
+  
+  
+  const leftButton = screen.getByText(/LEFT/);
+  userEvent.click(leftButton);
+  
+
+  const stepsElementUpdated = await screen.findByText(/You moved 1 time/);
+  expect(stepsElementUpdated).toBeInTheDocument();
+});
+test('renders the submit button', () => {
+  render(<AppFunctional />);
+
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  expect(submitButton).toBeInTheDocument();
+});
